6d4c9566698b55bfcc965da1f7db6f88fafca65a
unknown <jorovipe@gmail.com>
2018-05-12 12:21:59 -0500

    Canvas smoke backgroound implemented

--- js/app.js
+++ js/app.js
@@ -2,13 +2,24 @@ var container,
   renderer,
   scene,
   camera,
-  mesh,
+  material,
+  quad,
   texture,
   start = Date.now(),
   fov = 30;
 
+  var bufferScene;
+  var textureA;
+  var textureB;
+  var bufferMaterial;
+  var plane;
+  var bufferObject;
+  var finalMaterial;
+  var quad;
+
 // Ensures DOM is loaded
 window.addEventListener( 'load', function() {
+	
 	let queries = queryToJson();
 	
 	let sex = 'men';
@@ -22,24 +33,7 @@ window.addEventListener( 'load', function() {
 				sex = 'girl';
 			}
 		}
-	}
-
-
-	
-	// BACKGROUND LOGIC
-	container = document.getElementById('canvas-container');
-	// create a scene
-	scene = new THREE.Scene();
-
-	// create a camera the size of the browser window
-	// and place it 100 units away, looking towards the center of the scene
-	camera = new THREE.PerspectiveCamera(
-		fov,
-		window.innerWidth / window.innerHeight,
-		1,
-		10000
-	);
-	camera.position.z = 100;
+	}	
 	
 	// Loads the texture
 	if (sex === 'girl')
@@ -50,7 +44,9 @@ window.addEventListener( 'load', function() {
 	{
 		texture = new THREE.TextureLoader().load( '../img/man-bg1-scaled.jpg' );
 	}
-
+	
+	// Initialize the Threejs scene
+	sceneSetup();
 	
 	// create a wireframe material
 	/*material = new THREE.MeshBasicMaterial( {
@@ -58,54 +54,133 @@ window.addEventListener( 'load', function() {
 		//wireframe: true
 	} );*/
 
-	material = new THREE.ShaderMaterial( {
+	/*material = new THREE.ShaderMaterial( {
 	  vertexShader: document.getElementById( 'vertexShader' ).textContent,
 	  fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
 	  uniforms:
 	  {
-	  	res: { value: new THREE.Vector2(1920, 1080) },
-	  	tex: {value: texture}
+	  	res: { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+	  	tex: { type: 't', value: texture}
 	  }
 	} );
+	let geometry = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight);
 
-	let divider = 14;
 	// create a sphere and assign the material
-	mesh = new THREE.Mesh(
-		new THREE.PlaneGeometry( 1920/divider, 1080/divider, 32 ),
+	quad = new THREE.Mesh(
+		geometry,
 		material
 	);
-	scene.add( mesh );
+	scene.add( quad );*/
+
+	// FRAME BUFFER TRICK
+	bufferTextureSetup();
+	
+	// Like the update in unity
+	renderBuffer();
+
+});
+
+function bufferTextureSetup(){
+	//Create buffer scene
+	bufferScene = new THREE.Scene();
+	//Create 2 buffer textures
+	textureA = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+	textureB = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter} );
+	//Pass textureA to shader
+	bufferMaterial = new THREE.ShaderMaterial( {
+		uniforms: {
+			mouse: {type: 'v3', value: new THREE.Vector2(0, 0, 0)}, // mouse: x, y, isClicked
+			tex: { type: 't', value: texture},
+			bufferTexture: { type: "t", value: textureA },
+			res : {type: 'v2',value:new THREE.Vector2(window.innerWidth,window.innerHeight)}//Keeps the resolution
+		},
+		fragmentShader: document.getElementById( 'fragmentShader' ).innerHTML
+	} );
+	plane = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight );
+	bufferObject = new THREE.Mesh( plane, bufferMaterial );
+	bufferScene.add(bufferObject);
+
+	//Draw textureB to screen 
+	finalMaterial =  new THREE.MeshBasicMaterial({map: textureB});
+	quad = new THREE.Mesh( plane, finalMaterial );
+	scene.add(quad);
+}
+
+
+function sceneSetup() {
+	// BACKGROUND LOGIC
+	container = document.getElementById('canvas-container');
+	// create a scene
+	scene = new THREE.Scene();
+
+	var width = window.innerWidth;
+    var height = window.innerHeight;
+	// create a camera the size of the browser window
+	// and place it 100 units away, looking towards the center of the scene
+	camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+	camera.position.z = 100;
 
 	// create the renderer and attach it to the DOM
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setPixelRatio( window.devicePixelRatio );
-
 	container.appendChild( renderer.domElement );
-
-	render();
-
-});
+}
 
 // Renders the scene
 function render() {
-
   // let there be light
   renderer.render( scene, camera );
   requestAnimationFrame( render );
+}
 
+// Renders the scene frame buffer trick
+function renderBuffer() {
+	//Draw to textureB
+	renderer.render(bufferScene,camera,textureB,true);
+			
+	//Swap textureA and B
+	var t = textureA;
+	textureA = textureB;
+	textureB = t;
+	quad.material.map = textureB;
+	bufferMaterial.uniforms.bufferTexture.value = textureA;
+
+	update();
+
+	//Finally, draw to the screen
+	renderer.render( scene, camera );
+	requestAnimationFrame( renderBuffer );
+}
+
+function update()
+{
+	// Updates uniform
+	document.onmousemove = function(event){
+		UpdateMousePosition(event.clientX,event.clientY)
+	}
+}
+
+function UpdateMousePosition(X,Y){
+	var mouseX = X;
+	var mouseY = window.innerHeight - Y; // DOM Coordinate system is inverted
+	bufferMaterial.uniforms.mouse.value.x = mouseX;
+	bufferMaterial.uniforms.mouse.value.y = mouseY;
 }
 
 // Resizes canvas
 window.addEventListener('resize', function () {
+	// Updates the resolution uniform at quad shader
+	material.uniforms.res.value.x = window.innerWidth;
+	material.uniforms.res.value.y = window.innerHeight;
+
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 });
 
 // DECODES QUERY TO JSON
-function queryToJson()
-{	
+function queryToJson() {	
 	// example search ?sex=men&age=19
 	let searchStr = window.location.search; // this is a string
 	searchStr = searchStr.replace(/%20/g, ''); // Removes white spaces using regular expresions
